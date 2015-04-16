8a20aa4b4214f416269b8e5b57bf96251bb74661
P. Michael Furlong <furlong@gmail.com>
2015-04-15 18:08:09 -0700

    Updated the decision making rule for the foraging sampler.  Have to have a default improvement for the first time you encounter a symbol, otherwise you'll never encounter it again.  Also fixed some bugs in the simulation runner.

--- code/sampling.py
+++ code/sampling.py
@@ -1,5 +1,6 @@
 import numpy as np
 from collections import Counter
+from scipy.stats import gaussian_kde
 
 # Considering two different foraging algorithms.
 # The first is one that considers the arrival times of each
@@ -13,13 +14,138 @@ class ForagingSampling:
 		self.symb_arrival_time = {}
 		self.last_symbol_time = {}
 		self.symbol_frequency  = Counter()
+		self.current_time = 0.
+		self.symbol_iat = {}  # interarrival times.
+		self.last_time_seen = {}  # interarrival times.
+		self.cost = {}
+		self.rewards = {}
+		self.samples = {}
 	### end __init__
 
-	def query(self,time,symb):
-		pass
+	def dist_value(self,symbol):
+		# Consider predicting the next reward from the previous one.
+		# Should be able to learn a curve here.
+		return np.mean(self.rewards[symbol])/(np.mean(self.symbol_iat[symbol]) + np.mean(self.cost[symbol]))
+	### end value
+
+	def value(self,symbol):
+		return np.mean(self.rewards[symbol])/np.mean(self.cost[symbol])
+
+	def env_value(self):
+		# should this be:
+		# rate_{symb} = avg_{symb}[value]/(avg_{symb}[iat]+sampling_time)
+		# We want this one ultimately:
+		# avg_{symb}[value]/(avg_{symb}[iat]+avg_{symb}[sampling_time])
+		# should we then do an empirical average or a sum?
+		# The other alternative would be avg_{symb}[value]/(avg[iat] + avg_{symb}[sampling_time])
+		# where we learn the probability of which symbol we'd encounter at any given time.
+		num_keys = len(self.samples.keys())
+		result = np.mean([self.dist_value(key) for key in self.samples.keys()])
+		return result
+	### end env_value
+
+	def __call__(self,time,symb):
+		retval = None
+		self.current_time += time
+		self.last_time_seen[symb] = self.current_time
+		symbol_dt = self.last_time_seen[symb]
+		if symb not in self.samples.keys():
+			self.samples[symb] = []
+			self.rewards[symb] = []
+			self.symbol_iat[symb] = []
+
+			retval = 'sample'
+		else:
+			sampling_cost = np.mean(self.cost[symb])
+			symbol_value = self.value(symb)
+			env_value = self.env_value()
+# 			print symbol_value,sampling_cost, env_value
+			if symbol_value/sampling_cost >= env_value:
+				retval = 'sample'
+			### end
+
+			# What you'd need to do for all the symbols.
+			# update interarrival time
+			old_time = self.last_time_seen[symb]
+			# convert to zero for first time
+			symbol_dt = self.last_time_seen[symb] - old_time
+			### end if
+		### end
+		self.symbol_iat[symb].append(symbol_dt)
+
+		return retval
 	### end query
 
-	def update(self,time,symb,val):
-		pass
-	### update
+	def update(self,time,symb,val,cost):
+		# Compute reward
+		self.current_time += cost
+		# In the default case we have to show some improvement.
+		# Make it the same for all of them so they have the same
+		# starting point, why not?
+		before = lambda x: 0.0000001
+		after = lambda x: 0.00001
+		if len(self.samples[symb]) > 1:
+			try:
+				before = gaussian_kde(self.samples[symb])
+# 			except (RuntimeError,TypeError,NameError,ValueError):
+			except np.linalg.linalg.LinAlgError:
+				print 'Before singular matrix:'
+				print self.samples
+		### end if
+		self.samples[symb].append(val)
+		if len(self.samples[symb]) > 1:
+			try:
+				after = gaussian_kde(self.samples[symb])
+# 			except (RuntimeError,TypeError,NameError,ValueError):
+			except np.linalg.linalg.LinAlgError:
+				print 'After singular matrix:'
+				print self.samples
+		### end if
+
+		reward = np.log(after(val)/before(val))
+		print symb, reward
+		### end if
+		self.rewards[symb].append(reward)
+
+		# Update the cost of sampling
+		self.cost[symb] = cost
+# 		self.samples[symb].append(val)
+		# update time since last seen.
+		### end if
+
+	### end update
+
 ### end class ForagingSampling
+
+class UniformSampling:
+	def __init__(self):
+		self.symbols = []
+		self.distribution = Counter()
+		self.samples_taken = 0
+		self.samples= {}
+		self.current_time = 0.
+	### end __init__
+
+	def __call__(self,time,symb):
+		retval = None
+		self.current_time += time
+	
+		if len(self.distribution.values()) == 0:
+			retval = 'sample'
+		elif (self.distribution[symb] < max(self.distribution.values()) or \
+				  self.distribution[symb] == min(self.distribution.values()) ):
+			retval = 'sample'
+			self.samples_taken += 1
+		### end 
+		return retval
+	### end query
+
+	def update(self,time,symb,val,cost):
+		self.current_time += cost
+		self.distribution.update([symb])
+		if not symb in self.samples.keys():
+			self.samples[symb] = []
+		### end
+		#print symb, self.samples[symb]
+		self.samples[symb].append(val)
+	### end update
--- code/simulate-transect.py
+++ code/simulate-transect.py
@@ -1,21 +1,89 @@
 import numpy as np
 import os.path
 import sys
+from sampling import *
+import cPickle as pickle
+
+from scipy.integrate import quad
+from scipy.stats import gaussian_kde,norm
+
+import warnings
+
+#warnings.filterwarnings('error')
 
 
-def main(trial_name):
-	trial = np.loadtxt(trial_name)
+def score_integrand(x,p,q):
+	return np.abs(p.pdf(x)-q(x))
+
+def kl_integrand(x,p,q):
+# 	print p(x),q(x)
+	a = p.pdf(x)*np.log(p.pdf(x))
+	b = p.pdf(x)*np.log(q(x))
+
+	return a - b
+### end kl_integrand
+
+def entropy_integrand(x,p):
+	return -p.pdf(x)*np.log(p.pdf(x))
+
+
+def score(agent,truth):
+	symbols = truth.keys()
+	score = 0.
+	results = []
+	for symbol in symbols:
+		integral = None
+		tail_len = 4*truth[symbol].var()
+		true_mean = truth[symbol].mean()
+		int_range = [true_mean - tail_len,true_mean + tail_len]
+		samples = agent.samples[symbol]
+		# fit kde
+		try:
+			estimator = gaussian_kde(samples)
+		except ValueError:
+			print symbol, samples
+		# use the scipy integrator to compute the kl divergence.
+		integral = quad(score_integrand,int_range[0],int_range[1],args=(truth[symbol],estimator),limit=100)
+		#else:
+		#	# Compute the entropy of the true distribution and take all those 
+		#	# points 
+		#	integral = quad(entropy_integrand,int_range[0],int_range[1],args=(truth[symbol],))
+		### end if
+		score += integral[0]
+		results.append((symbol,len(agent.samples[symbol]),integral[0]))
+	### end for
+	return (results,score)
+### end score
+
+def main(trial_name,param_data_file):
+	sample_cost = 1e-1 # arbitrary number. 
+										 # If you scale the time range do the same here.
 	# load transect
+	trial = np.loadtxt(trial_name)
+	param_data = pickle.load(open(param_data_file,'rb'))
+	params = param_data[1]
+	truth = {k: norm(loc=params[k][0],scale=params[k][1]) for k in params.keys()}
 	# instantiate the agent
 	# run agent on transect
+# 	agent = UniformSampling()
+	agent = ForagingSampling()
 	for option in trial:
-		print option	
+		#print option	
+		time = option[0]
+		symbol = int(option[1])
+		value = option[2]
+		cost = sample_cost
+		action = agent(time,symbol)
+		if action == 'sample':
+			agent.update(time,symbol,value,cost)
+			# In the future with random costs:
+			#agent.update(option[1],option[2],option[3])
 	# compute error from true distributions.
-
-	
+	print score(agent,truth)
 
 ### end main
 
 if __name__=='__main__':
 	trial_name = os.path.abspath(os.path.expanduser(sys.argv[1]))
-	main(trial_name)
+	params_data = os.path.abspath(os.path.expanduser(sys.argv[2]))
+	main(trial_name,params_data)
