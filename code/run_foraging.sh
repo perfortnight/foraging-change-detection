507ac3158180df1f3f0a8d5b3e85f5c1a1e74a4b
furlong <furlong@faraday>
2015-04-22 07:55:39 -0700

    Added scripts to run experiments. Added data

---
+++ code/run_foraging.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./run_experiment.sh foraging ident-ident
+./run_experiment.sh foraging ident-diff
+./run_experiment.sh foraging fat1-ident
+./run_experiment.sh foraging skinny1-ident
+
